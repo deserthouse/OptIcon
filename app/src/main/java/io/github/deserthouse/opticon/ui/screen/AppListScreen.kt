61f24d5a0c835086c827faa5b738e490012d1061
澪狼 <deserthouse98@gmail.com>
2026-09-11 15:42:03 +0800

    docs: 0.6.4-alpha 版本记录

--- app/src/main/java/io/github/deserthouse/opticon/ui/screen/AppListScreen.kt
+++ app/src/main/java/io/github/deserthouse/opticon/ui/screen/AppListScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
